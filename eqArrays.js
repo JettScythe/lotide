034fdcbd10557db2415a771bcee069bad5a9fcc7
JettScythe <jettscythe@hotmail.com>
2021-04-27 15:31:11 -0300

    Added eqArrays + linting fix

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false); // => should FAIL
+assertEqual(eqArrays([1, 2, 4], [1, 2, 3]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [1, 'hello', 3]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [1, 'hello', 3]), false); // => should PASS
--- tail.js
+++ tail.js
@@ -1,5 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -7,15 +7,15 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const tail = function (arr) {
-  let newArr = arr.slice(1)
+const tail = function(arr) {
+  let newArr = arr.slice(1);
   return newArr;
 };
 
-// Test Case: Check the original array 
+// Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(JSON.stringify(result), JSON.stringify(["Lighthouse", "Labs"])); 
+assertEqual(JSON.stringify(result), JSON.stringify(["Lighthouse", "Labs"]));
