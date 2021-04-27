630362ed34f7fa77e65948cf36fc4a10defeec70
JettScythe <jettscythe@hotmail.com>
2021-04-27 17:12:34 -0300

    Added without using includes()
    Co-authored-by: Zoheb Boga <zohebboga@hotmail.com>

---
+++ without.js
@@ -0,0 +1,33 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const without = (source, itemsToRemove) => {
+  let newArr = [];
+  for (let val of source){
+    if(!itemsToRemove.includes(val)){
+      newArr.push(val)
+    }
+  }
+  console.log(newArr);
+};
+
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
