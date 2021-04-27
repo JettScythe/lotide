446702b25abacf6ce64b554b6552ee72abe148a7
JettScythe <jettscythe@hotmail.com>
2021-04-27 19:54:58 -0300

    Add Flatten, lint fixes

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const flatten = (arr) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        newArr.push(arr[i][j]);
+      }
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
+
+const array =  [1, 2, 3, 4, 5, 6];
+console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), array)); // => [1, 2, 3, 4, 5, 6]
+
--- without.js
+++ without.js
@@ -21,16 +21,16 @@ const assertArraysEqual = (arr1, arr2) => {
 
 const without = (source, itemsToRemove) => {
   let newArr = [];
-  for (let val of source){
-    if(!itemsToRemove.includes(val)){
-      newArr.push(val)
+  for (let val of source) {
+    if (!itemsToRemove.includes(val)) {
+      newArr.push(val);
     }
   }
   console.log(newArr);
-}; 
+};
 
-without([1, 2, 3], [1]) // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
@@ -38,12 +38,12 @@ assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 
 const without2 = (source, itemsToRemove) => {
-  let newArr = source.filter( val => !itemsToRemove.includes(val))
+  let newArr = source.filter(val => !itemsToRemove.includes(val));
   console.log(newArr);
-}
+};
 
-without2([1, 2, 3], [1]) // => [2, 3]
-without2(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+without2([1, 2, 3], [1]); // => [2, 3]
+without2(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
 const words2 = ["hello", "world", "lighthouse"];
 without2(words2, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
