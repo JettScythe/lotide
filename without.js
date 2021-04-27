22dc075876b8940f97150d7f9217c184f438cd10
JettScythe <jettscythe@hotmail.com>
2021-04-27 17:29:02 -0300

    Added alternative without method, added test cases

--- without.js
+++ without.js
@@ -27,7 +27,24 @@ const without = (source, itemsToRemove) => {
     }
   }
   console.log(newArr);
-};
+}; 
 
 without([1, 2, 3], [1]) // => [2, 3]
 without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+
+const without2 = (source, itemsToRemove) => {
+  let newArr = source.filter( val => !itemsToRemove.includes(val))
+  console.log(newArr);
+}
+
+without2([1, 2, 3], [1]) // => [2, 3]
+without2(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+const words2 = ["hello", "world", "lighthouse"];
+without2(words2, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words2, ["hello", "world", "lighthouse"]);
