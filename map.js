928b3987433aae1e2999169b010481ac84906d57
JettScythe <jettscythe@hotmail.com>
2021-04-29 14:56:39 -0300

    Added takeUntil assignment, linting fixes.

--- map.js
+++ map.js
@@ -21,7 +21,7 @@ const assertArraysEqual = (arr1, arr2) => {
 // Our map function will take in two arguments:
 // An array to map & a callback function
 
-/* The map function will return a new array based 
+/* The map function will return a new array based
 on the results of the callback function. */
 
 const words = ["ground", "control", "to", "major", "tom"];
@@ -33,10 +33,11 @@ const map = function(array, callback) {
     results.push(callback(item));
   }
   return results;
-}
+};
 const results1 = map(words, word => word[0]);
 const results2 = map(words2, word => word[0]);
 
 assertArraysEqual(['g', 'c', 't', 'm', 't'], results1); //should PASS
 assertArraysEqual([], results1); // should FAIL
 assertArraysEqual(results2, results1); // should PASS
+assertArraysEqual(['g'.toUpperCase(), 'c', 't', 'm', 't'], results1); // should FAIL
---
+++ takeUntil.js
@@ -0,0 +1,47 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const takeUntil = (array, callback) => {
+  let results = [];
+  for (const num of array) {
+    if (callback(num) === true) {
+      break;
+    } else {
+      results.push(num);
+    }
+  }
+  return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual([ "I've", 'been', 'to', 'Hollywood' ], results2); //should PASS
+assertArraysEqual([], results1); // should FAIL
+assertArraysEqual(results2, results1); // should FAIL
+assertArraysEqual([1,2,5,7,2], results1); // should PASS
