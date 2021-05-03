3b1cf5311ae6a00788e9d7063d06cd63d8f51d85
JettScythe <jettscythe@hotmail.com>
2021-05-03 15:46:40 -0300

    refactor flatten + tests

--- flatten.js
+++ flatten.js
@@ -1,23 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🚫🚫🚫 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const flatten = (arr) => {
   let newArr = [];
   for (let i = 0; i < arr.length; i++) {
@@ -34,6 +14,4 @@ const flatten = (arr) => {
 
 module.exports = flatten;
 
-const array =  [1, 2, 3, 4, 5, 6];
-console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), array)); // => [1, 2, 3, 4, 5, 6]
 
---
+++ test/flattenTest.js
@@ -0,0 +1,9 @@
+const flatten = require('../flatten');
+const {assert} = require('chai');
+
+describe('#flatten', () => {
+  it('should deep equal array', () => {
+    const array =  [1, 2, 3, 4, 5, 6];
+    assert.deepEqual(flatten([1, 2, [3, 4], 5, [6]]), array)
+  })
+})
