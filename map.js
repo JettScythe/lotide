91703e07dee02fc91d67909aac7ed44537ce51af
JettScythe <jettscythe@hotmail.com>
2021-05-03 15:58:59 -0300

    refactor map + tests

--- map.js
+++ map.js
@@ -1,32 +1,8 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🚫🚫🚫 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 // Our map function will take in two arguments:
 // An array to map & a callback function
 
 /* The map function will return a new array based
 on the results of the callback function. */
-
-const words = ["ground", "control", "to", "major", "tom"];
-const words2 = ["goofy", "citrus", "to", "massive", "tomato"];
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -36,10 +12,3 @@ const map = function(array, callback) {
 };
 
 module.exports = map;
-const results1 = map(words, word => word[0]);
-const results2 = map(words2, word => word[0]);
-
-assertArraysEqual(['g', 'c', 't', 'm', 't'], results1); //should PASS
-assertArraysEqual([], results1); // should FAIL
-assertArraysEqual(results2, results1); // should PASS
-assertArraysEqual(['g'.toUpperCase(), 'c', 't', 'm', 't'], results1); // should FAIL
---
+++ test/mapTest.js
@@ -0,0 +1,23 @@
+const map = require('../map');
+const {assert} = require('chai');
+
+describe('#map', () => {
+  it('should equal', () => {
+    const words = ["ground", "control", "to", "major", "tom"];
+    const results1 = map(words, word => word[0]);
+    assert.deepEqual(['g', 'c', 't', 'm', 't'], results1)
+  })
+  it('should not equal', () => {
+    const words = ["ground", "control", "to", "major", "tom"];
+    const results1 = map(words, word => word[0]);
+    assert.notDeepEqual([], results1)
+  })
+
+  it('should equal', () => {
+    const words = ["ground", "control", "to", "major", "tom"];
+    const words2 = ["goofy", "citrus", "to", "massive", "tomato"];
+    const results1 = map(words, word => word[0]);
+    const results2 = map(words2, word => word[0]);
+    assert.deepEqual(results1, results2);
+  })
+})
