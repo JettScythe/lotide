6cd722bf7ea4357253a9090e9e84a3500a7f74b1
JettScythe <jettscythe@hotmail.com>
2021-05-03 14:17:48 -0300

    refactor code, adding unit tests using mocha/chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,14 @@
-const assertEqual = require('../assertEqual');
+const { assert } = require('chai');
 const head = require('../head');
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
+describe('#head', () => {
+  it('Should return 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("should returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it('should return hello for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello');
+  })
+})
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,23 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const { assert, expect } = require('chai');
 
 //TESTS
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), middle([12, 3, 4, 99]));
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [true, 4]);
-assertArraysEqual(middle([1, 2, 'hello', 4, 5, 6]), [3, 4]);
+describe('#middle', () => {
+  it('should return [3, 4] for [1, 2, 3, 4, 5, 6]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  });
+
+  it('should return [3, 4] when comparing middle([1, 2, 3, 4, 5, 6]) and middle([12, 3, 4, 99])', () => {
+    const result = middle([1, 2, 3, 4, 5, 6]);
+    const result1 = middle([12, 3, 4, 99]);
+    expect(result).to.deep.equal(result1);
+  });
+  
+  it('should return false when comparing arrays with unmatched data types', () => {
+    assert.notDeepEqual(middle([1, 2, 3, 4, 5, 6]), [true, 4]);
+  });
+
+  it('should return false when comparing arrays with unmatched middles', () => {
+    assert.notDeepEqual(middle([1, 2, 'hello', 4, 5, 6]), [3, 4]);
+  });
+});
