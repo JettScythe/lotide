2b0485fc9a9faa3f51fcb781a0c8fc542a0b0015
JettScythe <jettscythe@hotmail.com>
2021-05-03 15:50:17 -0300

    refactor letter positions + test

--- letterPositions.js
+++ letterPositions.js
@@ -18,27 +18,3 @@ const letterPositions = (sentence)  => {
 };
 
 module.exports = letterPositions;
-
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🚫🚫🚫 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-console.log(letterPositions("Lighthouse in the house"));
-assertArraysEqual(letterPositions("hello").l, [2, 3]);
---
+++ test/letterPositionsTest.js
@@ -0,0 +1,8 @@
+const letterPositions = require('../letterPositions');
+const {assert} = require('chai');
+
+describe('#letterPositions', () => {
+  it('should return letter positions of 2, 3 when given (hello).l', () => {
+    assert.deepEqual(letterPositions("hello").l, [2, 3]);
+  })
+})
