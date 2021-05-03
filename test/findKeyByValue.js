0bbe7c7982fba9eaca404d61ab0fcb171d11a3bc
JettScythe <jettscythe@hotmail.com>
2021-05-03 16:11:47 -0300

    move unfinished tests to test file

--- test/findKeyByValue.js
+++ test/findKeyByValue.js
@@ -9,5 +9,11 @@ describe('#findKeyByValue', () => {
       drama:  "The Wire"
     };
     assert.equal(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama")
+    // assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+    // assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "drama");
+    // assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "sci_fi");
+    // assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+    // assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), "comedy");
+    // assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), 5);
   })
 })
