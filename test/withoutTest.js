49a3c3c4b54110ac89ad318b6251c266862668e5
JettScythe <jettscythe@hotmail.com>
2021-05-03 16:10:00 -0300

    without refactor + tests

---
+++ test/withoutTest.js
@@ -0,0 +1,12 @@
+const without = require('../without');
+const {assert} = require('chai');
+
+describe('#without', () => {
+  it('should not modify the original array', () => {
+    const words2 = ["hello", "world", "lighthouse"];
+    without(words2, ["lighthouse"]);
+    assert.deepEqual(words2, ["hello", "world", "lighthouse"])
+    without([1, 2, 3], [1]); // => [2, 3]
+    without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+  })
+})
--- without.js
+++ without.js
@@ -1,52 +1,6 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🚫🚫🚫 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const without = (source, itemsToRemove) => {
-  let newArr = [];
-  for (let val of source) {
-    if (!itemsToRemove.includes(val)) {
-      newArr.push(val);
-    }
-  }
-  console.log(newArr);
+  let newArr = source.filter(val => !itemsToRemove.includes(val));
+  return newArr;
 };
-
 module.exports = without;
 
-without([1, 2, 3], [1]); // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
-
-const without2 = (source, itemsToRemove) => {
-  let newArr = source.filter(val => !itemsToRemove.includes(val));
-  console.log(newArr);
-};
-
-without2([1, 2, 3], [1]); // => [2, 3]
-without2(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
-const words2 = ["hello", "world", "lighthouse"];
-without2(words2, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words2, ["hello", "world", "lighthouse"]);
